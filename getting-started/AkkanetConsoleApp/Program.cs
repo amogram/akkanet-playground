3ed7f12841ae0495780f652a4513b8e9020d342c
Andrew McCaughan <andrew.mccaughan@gmail.com>
2015-04-09 10:39:29 +0100

    http://getakka.net/docs/Getting%20started

---
+++ getting-started/AkkanetConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Akka.Actor;
+
+namespace AkkanetConsoleApp
+{
+    public class Greet
+    {
+        public string Who { get; private set; }
+
+        public Greet(string who)
+        {
+            Who = who;
+        }
+    }
+
+    public class GreetingActor : ReceiveActor
+    {
+        public GreetingActor()
+        {
+            Receive<Greet>(greet => Console.WriteLine("Hello {0}", greet.Who));
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var system = ActorSystem.Create("MySystem");
+
+            var greeter = system.ActorOf<GreetingActor>("greeter");
+            greeter.Tell(new Greet("World"));
+
+            Console.ReadLine();
+        }
+    }
+}
